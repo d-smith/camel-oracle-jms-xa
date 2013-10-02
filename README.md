f9e2562f301246bedf25e1c503d90aa1481acf93
d-smith <doug.smith.mail@gmail.com>
2013-10-02 10:37:35 -0600

    updated route for concurrent consumers

--- README.md
+++ README.md
@@ -22,3 +22,17 @@ If you are using Oracle XA, there is [additional configuration](http://www.atpea
 
 Finally, you'll need plenty of head room when you run the sample, so a JVM 
 option of -Xmx2014m would be appropriate.
+
+Note the sample can also be used in Fuse ESB. Simple install the following
+bundles:
+
+
+    install -s mvn:org.springframework/spring-jdbc/3.1.3.RELEASE
+    install -s wrap:mvn:com.oracle/ojdbc6/11.1.0
+    install -s wrap:mvn:com.oracle/aqapi/11.1
+    install -s mvn:com.atomikos/transactions-osgi/3.8.0
+    install -s mvn:com.atomikos/transactions-jdbc/3.8.0
+    install -s mvn:com.atomikos/transactions-jms/3.8.0
+    install -s mvn:camel-oracle/camel-oracle/1.0-SNAPSHOT
+
+
--- src/main/java/sample/route/SampleRoute.java
+++ src/main/java/sample/route/SampleRoute.java
@@ -4,9 +4,13 @@ import org.apache.camel.builder.RouteBuilder;
 
 
 public class SampleRoute extends RouteBuilder {
+    private static final String COMPONENT_CONFIG =
+            "?connectionFactory=AQconnectionFactory" +
+                    "&transacted=true&transactionManager=#jtaTransactionManager" +
+                    "&concurrentConsumers=5";
     @Override
     public void configure() throws Exception {
-        from("jms:queue:batch.sample_q?connectionFactory=AQconnectionFactory&transacted=true&transactionManager=#jtaTransactionManager")
+        from("jms:queue:batch.sample_q" + COMPONENT_CONFIG)
                 .to("bean:inserter")
                 .to("bean:flakey")
                 .to("log:sample.log?level=INFO");
